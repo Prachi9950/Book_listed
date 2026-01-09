d9efd901ff122122d3d1e44c37ea69009a7a78e5
Prachi9950 <prachichoudhary031@gmail.com>
2026-01-09 22:12:15 +0530

    script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const form = document.getElementById("form-list");
 const alertBox = document.getElementById("alert-box");
 
 class Book {
-  constructor(title, author, isbn, dateAdded) {
+  constructor(title, author, bookNo, date) {
     this.title = title;
     this.author = author;
-    this.bno = bno;
+    this.bookNo = bookNo;
     this.date = date;
   }
 }
@@ -35,9 +35,9 @@ form.addEventListener("submit", (e) => {
 
   const title = document.getElementById("title").value.trim();
   const author = document.getElementById("author").value.trim();
-  const bno = document.getElementById("bno").value.trim();
+  const bookNo = document.getElementById("bookno").value.trim();
 
-  if (title === "" || author === "" || isbn === "") {
+  if (title === "" || author === "" || bookNo === "") {
     showAlert("Please fill all fields!", "error");
   } else {
     const today = new Date();
@@ -46,11 +46,10 @@ form.addEventListener("submit", (e) => {
       month: "short",
       year: "numeric",
     });
-    const book = new Book(title, author, bno, formattedDate);
 
+    const book = new Book(title, author, bookNo, formattedDate);
     Store.addBook(book);
     showAlert("New Book Added!", "success");
     form.reset();
   }
 });
-
